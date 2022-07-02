b7753aca4bd71dbc3ef272cca1e3417232fd3a51
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-07-01 21:18:55 -0700

    chore: actualizacao da pagina de Registro"

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,7 +5,8 @@ const routes = [
     component: () => import('layouts/LoginLayout.vue'),
     children: [
       { path: '', name: 'loginDefault', component: () => import('pages/Login.vue') },
-      { path: 'login', name: 'login', component: () => import('pages/Login.vue') }
+      { path: 'login', name: 'login', component: () => import('pages/Login.vue') },
+      { path: 'register', name: 'register', component: () => import('pages/Register.vue') }
     ]
   },
   {
